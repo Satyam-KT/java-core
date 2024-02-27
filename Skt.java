aeab248dee26e321f5af775dbef21ebebdd4d1f3
Satyam-KT <kumarsatyamara1082@gmail.com>
2024-02-27 10:08:27 +0530

    committed

---
+++ Skt.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Skt{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		String s = "";
+		System.out.println("enter your name");
+		s = sc.next();
+		
+		System.out.println("Hello " + s);
+
+	}
+	}	
