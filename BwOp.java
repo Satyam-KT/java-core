d7383c83e58bb7cd7c5c7fa7f83f35b89952dd4d
Satyam-KT <kumarsatyamara1082@gmail.com>
2024-02-28 10:49:44 +0530

    committed

---
+++ BwOp.java
@@ -0,0 +1,10 @@
+class BwOp{
+	public static void main(String args[]){
+		int a = 10, b = 7;
+		System.out.println(a&b);
+		System.out.println(a|b);
+		System.out.println(a^b);
+		System.out.println(a>>b);
+		System.out.println(a<<b);
+
+	}}
