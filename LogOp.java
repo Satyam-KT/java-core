3536fa3c51978219e448de40c343ba828e9fb71a
Satyam-KT <kumarsatyamara1082@gmail.com>
2024-02-28 10:29:22 +0530

    Committed

---
+++ LogOp.java
@@ -0,0 +1,7 @@
+class LogOp {
+	public static void main(String args[]){
+		int a = 10, b = 5, c = 16;
+		System.out.println((a>b)&& (a>c));
+		System.out.println((a<b)|| (a>c));
+		System.out.println((b>c)&& (a<c));
+	}}
