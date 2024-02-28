87602397323f8cacfbbadc1290d9d64a1f1dfc9b
Satyam-KT <kumarsatyamara1082@gmail.com>
2024-02-28 10:35:44 +0530

    c

--- RelOp.java
+++ RelOp.java
@@ -1,9 +1,11 @@
-class Rel_op{
+class RelOp{
 	public static void main(String args[]){
 		int a = 10, b = 15, c =5;
 		System.out.println(a>b);
 		System.out.println(a<b);
 		System.out.println(a!=b);
 		System.out.println(a==c);
+		c = (a>b)?a:b;
+		System.out.println(c);
 
 	}}
