e40141b2dea4e5e5a5e3f2a4e0772f7471fd1c86
Satyam-KT <kumarsatyamara1082@gmail.com>
2024-02-29 11:40:26 +0530

    committed

---
+++ JLoops.java
@@ -0,0 +1,17 @@
+class JLoops{
+	public static void main(String args[]){
+		int n=1,m=1;
+		while(n<=10){
+			System.out.println(n);
+			n++;
+		}
+
+		do{
+			System.out.println(m);
+			m++;
+		}while(m<=10);
+
+		for(int i = 0; i<= 10; i++){
+			System.out.println(i);
+		}
+}}
