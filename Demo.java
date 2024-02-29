d170cb93376f9e44354cac94dba0fd5a5e139421
Satyam-KT <kumarsatyamara1082@gmail.com>
2024-02-29 10:48:20 +0530

    commit

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 class Demo{
 	public static void main(String args[]){
-		int num;
+		/*int num;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter a number");
 		num = sc.nextInt();
@@ -19,4 +19,29 @@ class Demo{
 			break;
 			default:
 			System.out.println("invalid");
+			}}}*/
+		char letter;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a letter");
+		letter = sc.next().charAt(0);
+		
+		switch(letter){
+			case 'a':
+			System.out.println("vowel");
+			break;
+			case 'e':
+			System.out.println("vowel");
+			break;
+			case 'i':
+			System.out.println("vowel");
+			break;
+			case 'o':
+			System.out.println("vowel");
+			break;
+			case 'u':
+			System.out.println("vowel");
+			break;
+			default :
+			System.out.println("consonant");
 			}}}
+			
